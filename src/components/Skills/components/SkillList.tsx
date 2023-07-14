71f0b0349388f17bb111173b4479c9ad48457c5a
Andryshka16 <a.lapchik7@gmail.com>
2023-07-14 18:15:36 +0300

    Contact me design

--- src/components/Skills/components/SkillList.tsx
+++ src/components/Skills/components/SkillList.tsx
@@ -9,15 +9,15 @@ interface SkillListProps {
 }
 
 const SkillList = ({ title, skills }: SkillListProps) => (
-    <div className='w-[420px] m-auto bg-[#2A2A67] rounded-[32px] px-12 pb-6'>
-        <h1 className='my-5 text-center font-bold text-4xl text-white'>{title}</h1>
+    <div className='m-auto w-[420px] rounded-[32px] bg-[#2A2A67] px-12 pb-6'>
+        <h1 className='my-5 text-center text-4xl font-bold text-white'>{title}</h1>
         <div className='m-auto grid grid-cols-2 gap-y-2.5'>
             {skills.map(({ skill, level }) => (
-                <div className='flex gap-2'>
+                <div className='flex gap-2' key={skill}>
                     <IoIosCheckmarkCircle size={20} color='#3EB9ED' className='mt-1' />
-                    <div className='w-fit h-fit'>
-                        <h1 className='text-xl text-white font-bold'>{skill}</h1>
-                        <h3 className='text-md text-white font-bold text-opacity-50'>{level}</h3>
+                    <div className='h-fit w-fit'>
+                        <h1 className='text-xl font-bold text-white'>{skill}</h1>
+                        <h3 className='text-md font-bold text-white text-opacity-50'>{level}</h3>
                     </div>
                 </div>
             ))}
